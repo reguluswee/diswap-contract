d8497b3d1023748b7dcf279965efa563d72b89a6
baiqi <495199985@qq.com>
2024-01-04 23:45:42 +0800

    新增配置等

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -8,12 +8,10 @@ dotenv.config();
 // tasks
 import "./src/tasks/accounts";
 
+const ACCOUNTS = [""]
+const INFURA_KEY = "3a487e3317dd401caff0ec9033079989"
+
 const config: HardhatUserConfig = {
-  networks: {
-    hardhat: {
-      blockGasLimit: 30000000,
-    },
-  },
   solidity: {
     compilers:[
       {
@@ -53,6 +51,76 @@ const config: HardhatUserConfig = {
         },
       }
     ],
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  networks: {
+    hardhat: {
+      blockGasLimit: 30000000,
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${INFURA_KEY}`,
+      accounts: ACCOUNTS
+    },
+    goerli: {
+      url: `https://goerli.infura.io/v3/${INFURA_KEY}`,
+      accounts: ACCOUNTS
+    },
+    polygon: {
+      url: `https://polygon-mainnet.infura.io/v3/${INFURA_KEY}`,
+      accounts: ACCOUNTS
+    },
+    polygonMumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${INFURA_KEY}`,
+      accounts: ACCOUNTS
+    },
+    optimism: {
+      url: `https://optimism-mainnet.infura.io/v3/${INFURA_KEY}`,
+      accounts: ACCOUNTS
+    },
+    arbitrum: {
+      url: `https://arbitrum-mainnet.infura.io/v3/${INFURA_KEY}`,
+      accounts: ACCOUNTS
+    },
+    linea: {
+      url: `https://linea-mainnet.infura.io/v3/${INFURA_KEY}`,
+      accounts: ACCOUNTS
+    },
+    zora: {
+      url: `https://rpc.zora.energy`,
+      accounts: ACCOUNTS
+    },
+    base: {
+      url: `https://developer-access-mainnet.base.org`,
+      accounts: ACCOUNTS
+    },
+    ganache: {
+      url: `HTTP://127.0.0.1:8545`,
+      accounts: ["c4d4119e5379948057e06ba17aa1a6523191beacfe0d70f2a28e04801fb32683","7dab2ba7c0260fd2c337d8f546aa5123ef0edf9d48b984c2df4f2ef81fe860c0","c30cb72618043bbc6586f60372ffa2ae399d2301982707b3f19a457149238d07"]
+    }
+  },
+  etherscan: {
+    apiKey: {
+      mainnet: "5DQX3VQ8B3K7SZPJXCH6WV6AACS6PX2676",
+      goerli: "5DQX3VQ8B3K7SZPJXCH6WV6AACS6PX2676",
+      optimisticEthereum: "8BZPGY1VAPCT5QV4ZR8SFG1TUHU3ED7HAW",
+      base:"DAF2QYN573FTR1A7BW83S6SRA1FXG374VW"
+      // arbitrumOne: "YOUR_ARBISCAN_API_KEY",
+    },
+    customChains: [
+      {
+        network: "zora",
+        chainId: 7777777,
+        urls: {
+          apiURL: "https://explorer.zora.energy/api",
+          browserURL: "https://explorer.zora.energy"
+        }
+      }
+    ]
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
---
+++ scripts/deploy_univ2.js
@@ -0,0 +1,40 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const { ethers } = require("hardhat");
+
+//npx hardhat run scripts/deploy.js --network ganache
+//验证方式一
+//npx hardhat flatten ./contracts/Contract.sol > ./flatten/Contract_zot_flatten.sol
+//验证方式二
+//npx hardhat verify --network goerli 0xdA35C2e65143262FfC2ef608Ad341821af55fb42
+
+//npx hardhat run scripts/dormy/deploy_dormy.js --network polygonMumbai
+async function main() {
+
+    const [owner] = await ethers.getSigners();
+
+    // 部署 WETH 合约
+    const WDIS = await ethers.getContractFactory("WDIS");
+    const wids = await WDIS.deploy();
+    console.log("TST deployed to:", weth.target);
+
+    const UniswapV2Factory = await ethers.getContractFactory("UniswapV2Factory");
+    const uniswapV2Factory = await UniswapV2Factory.deploy(owner.address);
+
+    console.log('UniswapV2Factory deployed to:', uniswapV2Factory.target);
+
+    const UniswapV2Router02 = await ethers.getContractFactory("UniswapV2Router02");
+    const uniswapV2Router02 = await UniswapV2Router02.deploy(uniswapV2Factory.target, wids.target);
+
+    console.log('UniswapV2Router02 deployed to:', uniswapV2Router02.target);
+
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
